a680ee5e9a2086b89e2fd89b8272c7807e55258f
zerolzhy <44442503+zerolzhy@users.noreply.github.com>
2020-09-04 10:49:25 -0400

    Added Formula Entitiy

--- src/main/java/com/lian/group/Entity/Project.java
+++ src/main/java/com/lian/group/Entity/Project.java
@@ -35,7 +35,12 @@ public class Project {
     @JoinColumn(name = "item_id")
     private Item item;
 
-
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            mappedBy = "project",
+            cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    Set<Formula> formulas;
 
     public int getId() {
         return id;
