8c7ce74e2b6e7160a1d2143e7890e4e0a938a770
zerolzhy <44442503+zerolzhy@users.noreply.github.com>
2020-09-08 18:32:36 -0400

    UserService

--- src/main/java/com/lian/group/Service/UserService.java
+++ src/main/java/com/lian/group/Service/UserService.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface UserService  {
     User findOne(Integer userId) throws Exception;
     List<User> findAll();
+    User registerNewUser(String username, String password, String email);
     UserDetails loadUserByUsername(String username);
 }
