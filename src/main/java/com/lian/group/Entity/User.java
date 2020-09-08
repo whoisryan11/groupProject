18b8616cd620713d969fd50cd4db711b6cc19307
zerolzhy <44442503+zerolzhy@users.noreply.github.com>
2020-09-08 16:29:15 -0400

    Added Spring Security

--- src/main/java/com/lian/group/Entity/User.java
+++ src/main/java/com/lian/group/Entity/User.java
@@ -1,11 +1,17 @@
 package com.lian.group.Entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -23,6 +29,15 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    //@Builder.Default
+    //private UserRole userRole = UserRole.USER;
+
+    //@Builder.Default
+    //private Boolean locked = false;
+
+    //@Builder.Default
+    //private Boolean enabled = false;
+
     @OneToMany(
             fetch=FetchType.LAZY,
             mappedBy = "user",
@@ -30,29 +45,17 @@ public class User {
                     CascadeType.DETACH, CascadeType.REFRESH})
     Set<Project> projects;
 
-    public Integer getId() {
-        return id;
-    }
+    public Integer getId() { return id; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public void setId(Integer id) { this.id = id; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername() { return username; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) { this.password = password; }
 
     public String getEmail() { return email; }
 
@@ -61,4 +64,30 @@ public class User {
     public UserDetail getUserDetail() { return userDetail; }
 
     public void setUserDetail(UserDetail userDetail) { this.userDetail = userDetail; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        final SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("USER");
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/lian/group/SecurityConfiguration.java
+++ src/main/java/com/lian/group/SecurityConfiguration.java
@@ -1,18 +1,40 @@
 package com.lian.group;
 
+import com.lian.group.ServiceImpl.UserServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserServiceImpl userService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                .and().formLogin();    }
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
